04c98bc7bdd97ac33be5dc966f692b32549ea69b
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2024-12-21 18:05:27 -0700

    Create function to allow user to create grid
    Add event listener to button, prompts the user for number for grid
    Created function that handles logic for grid creation

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 
 
-const container = document.querySelector('container');
+const container = document.querySelector('.container');
+
 function createGrid(number){
     
-    for(let i = 0; i < number; i++){
-        const gridItem = document.createElement('div');
+    document.querySelectorAll('.grid').forEach( n => n.remove());
+    
+
+    for(let i = 0; i < number * number; i++){
+        let gridItem = document.createElement('div');
+        gridItem.classList.add('grid');
+        const measure = (500 / number);
+        gridItem.setAttribute('style', `width:${measure}px; height:${measure}px;`);
+        console.log(gridItem);
         container.appendChild(gridItem);
     }
 }
+
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    const newGrid = prompt('Insert number to create new grid, limit 100');
+    createGrid(parseInt(newGrid));
+})
