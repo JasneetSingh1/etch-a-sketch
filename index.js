657e3fdf6f555a8da7d1812b8c065768f6f150f5
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2024-12-21 18:42:25 -0700

    Create Etch a Sketch function on the grid
    Created function that listens to when mouse enter event and changes
    the grid's colour from white to black. leaving a pixelated trail through
    the grid

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 
 
 const container = document.querySelector('.container');
+const button = document.querySelector('button');
 
 function createGrid(number){
-    
     document.querySelectorAll('.grid').forEach( n => n.remove());
     
-
     for(let i = 0; i < number * number; i++){
         let gridItem = document.createElement('div');
         gridItem.classList.add('grid');
         const measure = (500 / number);
         gridItem.setAttribute('style', `width:${measure}px; height:${measure}px;`);
-        console.log(gridItem);
         container.appendChild(gridItem);
     }
+
+    sketch()
 }
 
-
-const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
     const newGrid = prompt('Insert number to create new grid, limit 100');
     createGrid(parseInt(newGrid));
+    
 })
+
+
+function sketch(){
+    const grid = document.querySelectorAll('.grid');
+
+    grid.forEach(function(elem){
+        elem.addEventListener('mouseenter', function(){
+            elem.style.backgroundColor = 'black';
+        })
+    })
+}
+
+sketch()
